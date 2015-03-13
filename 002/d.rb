11aadc21595574f3c798140b4bda0dd42d066a65
YuZakuro <zakuro@yuzakuro.me>
2015-03-13 23:49:18 +0900

    update

--- 002/d.rb
+++ 002/d.rb
@@ -1,16 +1,16 @@
 a, d = gets.split.map(&:to_f)
 n = gets.to_i
-1..n.times do |i|
-  x = d * (i + 1) % (4 * a)
+(1..n).each do |i|
+  x = d * i % (4 * a)
   case
   when x < a
-    printf("%f %f", x, 0)
+    puts("#{x} 0")
   when x < 2 * a
-    printf("%f %f", a, x - a)
+    printf("#{a} #{x - a}", a, x - a)
   when x < 3 * a
-    printf("%f %f", a - (x - 2 * a), a)
+    printf("#{a - (x - 2 * a)} #{a}")
   else
-    printf("%f %f", 0, a - (x - 3 * a))
+    printf("0 #{a - (x - 3 * a)}")
   end
   puts
 end
